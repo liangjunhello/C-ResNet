4c5ad4291c47d5b2d92a61ddad2c453978c55596
liangjunhello <44309832+liangjunhello@users.noreply.github.com>
2020-06-07 21:10:23 +0800

    Update main_v3.py

--- main_v3.py
+++ main_v3.py
@@ -36,8 +36,6 @@ parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
                     help='number of data loading workers (default: 0)')
 parser.add_argument('--epochs', default=500, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
 parser.add_argument('-bs', '--batch-size', default=32, type=int,
                     metavar='N',
                     help='mini-batch size (default: 32)')
